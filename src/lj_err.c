f3f61bfa6ffb0d8b556311d0a6a4a472a6005052
Vlad Vissoultchev <wqweto@gmail.com>
2016-03-05 15:30:11 +0200

    missing `PUNWIND_HISTORY_TABLE` redeclaration

--- src/lj_err.c
+++ src/lj_err.c
@@ -403,6 +403,35 @@ static void err_raise_ext(int errcode)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#if !defined(UNWIND_HISTORY_TABLE_SIZE)
+typedef EXCEPTION_DISPOSITION (*PEXCEPTION_ROUTINE) (
+  struct _EXCEPTION_RECORD *ExceptionRecord,
+  PVOID EstablisherFrame,
+  struct _CONTEXT *ContextRecord,
+  PVOID DispatcherContext
+);
+#define UNWIND_HISTORY_TABLE_SIZE 12
+typedef struct _UNWIND_HISTORY_TABLE_ENTRY {
+  ULONG64 ImageBase;
+  PRUNTIME_FUNCTION FunctionEntry;
+} UNWIND_HISTORY_TABLE_ENTRY, *PUNWIND_HISTORY_TABLE_ENTRY;
+typedef struct _UNWIND_HISTORY_TABLE {
+  ULONG Count;
+  UCHAR Search;
+  ULONG64 LowAddress;
+  ULONG64 HighAddress;
+  UNWIND_HISTORY_TABLE_ENTRY Entry[UNWIND_HISTORY_TABLE_SIZE];
+} UNWIND_HISTORY_TABLE, *PUNWIND_HISTORY_TABLE;
+VOID NTAPI RtlUnwindEx(
+  PVOID TargetFrame,
+  PVOID TargetIp,
+  PEXCEPTION_RECORD ExceptionRecord,
+  PVOID ReturnValue,
+  PCONTEXT OriginalContext,
+  PUNWIND_HISTORY_TABLE HistoryTable
+);
+#endif
+
 /* Taken from: http://www.nynaeve.net/?p=99 */
 typedef struct UndocumentedDispatcherContext {
   ULONG64 ControlPc;
